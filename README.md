3429762a6ad56c876aa68d42d4ccac594631a55b
maelemonides <mael.emonides@gmail.com>
2022-11-25 11:52:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react_td
+ calendrier react
---
+++ src/const.js
@@ -0,0 +1,9 @@
+
+export const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+export const MOCKAPPS = [
+    { date: new Date(2022, 10, 25), title: "rendre projet react", color: "#238783" },
+    { date: new Date(2022, 10, 27), title: "rdv docteur", color: "#708898" },
+    { date: new Date(2022, 11, 18), title: "prendre le train", color: "#047106" },
+    { date: new Date(2022, 11, 24), title: "Noel", color: "#371395" }
+];
